5e8c1c8a1eef09447f89ea12ece930629bf52677
MuresanIA <muresan.alexandru933@gmail.com>
2021-04-24 19:17:16 +0300

    cool stuff

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { ErrorComponent } from "./assignment-error-message/error.component";
+import { SuccessComponent } from "./assignment-success-message/success.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    ErrorComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule,
