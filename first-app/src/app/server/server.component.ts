cb37090ad0a762f4fd1f6f9b4aa7755698b10535
MuresanIA <alexandru.muresan@restartenergy.ro>
2021-05-05 10:09:03 +0300

    saving progress #2

--- first-app/src/app/server/server.component.ts
+++ first-app/src/app/server/server.component.ts
@@ -2,11 +2,16 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "app-server",
-    templateUrl: "./server.component.html"
+    templateUrl: "./server.component.html",
+    styles: [`
+        .online {
+            color: white;
+        }
+    `]
 })
 export class ServerComponent {
     serverId: Number  = 10;
-    serverStatus: String  = "offline";
+    serverStatus: String  = 'offline';
 
     constructor(){
         this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
