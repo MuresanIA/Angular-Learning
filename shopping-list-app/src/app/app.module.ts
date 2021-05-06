d1d4f95ec3b76d3079f4895ab32835ab876d210b
MuresanIA <muresan.alexandru933@gmail.com>
2021-05-06 19:21:02 +0300

    Saving progress #3

--- shopping-list-app/src/app/app.module.ts
+++ shopping-list-app/src/app/app.module.ts
@@ -4,21 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-// import { RecipesComponent } from './recipes/recipes.component';
-// import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-// import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-// import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-// import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    // RecipesComponent,
-    // RecipeDetailComponent,
-    // RecipeItemComponent,
-    // ShoppingListComponent,
-    // ShoppingEditComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeListComponent,
+    RecipesComponent,
+    ShoppingEditComponent,
+    ShoppingListComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
