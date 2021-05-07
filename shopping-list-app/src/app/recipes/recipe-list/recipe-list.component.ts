61c894849a43216a72ae89cfab5a14abce951b52
MuresanIA <muresan.alexandru933@gmail.com>
2021-05-07 16:57:58 +0300

    saving progress #8

--- shopping-list-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping-list-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,9 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test', 'https://www.blondelish.com/wp-content/uploads/2020/04/Keto-Stuffed-Cabbage-Rolls-Gluten-Free-Low-Carb-Recipe-10.jpg'),
     new Recipe('A test recipe', 'This is simply a test', 'https://www.blondelish.com/wp-content/uploads/2020/04/Keto-Stuffed-Cabbage-Rolls-Gluten-Free-Low-Carb-Recipe-10.jpg')
+  
   ];
 
   constructor() { }
