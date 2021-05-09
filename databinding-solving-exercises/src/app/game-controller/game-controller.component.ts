20e076a6000ccaf457c9ad74d931991b1bf52218
MuresanIA <muresan.alexandru933@gmail.com>
2021-05-09 22:45:31 +0300

    saving progress #15

--- databinding-solving-exercises/src/app/game-controller/game-controller.component.ts
+++ databinding-solving-exercises/src/app/game-controller/game-controller.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import * as EventEmitter from 'events';
 
 @Component({
   selector: 'app-game-controller',
   templateUrl: './game-controller.component.html',
-  styleUrls: ['./game-controller.component.css']
+  styleUrls: ['./game-controller.component.css'],
 })
 export class GameControllerComponent implements OnInit {
+  intervalFired = new EventEmitter<number>();
+  interval;
+  lastNumber = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onStartGame() {
+    this.interval = setInterval(() => {
+      this.intervalFired.emit(this.lastNumber + 1);
+      this.lastNumber++;
+    }, 1000);
   }
-
 }
