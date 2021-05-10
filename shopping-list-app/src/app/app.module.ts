f2b19649d46c57bd74f2c40c3a82491ed0f583a3
MuresanIA <muresan.alexandru933@gmail.com>
2021-05-10 20:36:22 +0300

    saving progress #19

--- shopping-list-app/src/app/app.module.ts
+++ shopping-list-app/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -25,6 +26,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
