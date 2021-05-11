f40df2376dc162b15b302b7977fa52c952e6940d
MuresanIA <muresan.alexandru933@gmail.com>
2021-05-11 21:00:49 +0300

    saving progress #20

--- shopping-list-app/src/app/app.module.ts
+++ shopping-list-app/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
@@ -21,7 +22,8 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
     RecipeListComponent,
     RecipesComponent,
     ShoppingEditComponent,
-    ShoppingListComponent,  
+    ShoppingListComponent,
+    DropdownDirective  
   ],
   imports: [
     BrowserModule,
