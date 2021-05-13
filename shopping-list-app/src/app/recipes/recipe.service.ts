6b05e43ce8488472a4ee3ecabba95441891d49e1
MuresanIA <muresan.alexandru933@gmail.com>
2021-05-13 15:45:24 +0300

    Adding Ingredients to Recipes

--- shopping-list-app/src/app/recipes/recipe.service.ts
+++ shopping-list-app/src/app/recipes/recipe.service.ts
@@ -1,12 +1,25 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
 recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A test recipe', 'This is simply a test', 'https://www.blondelish.com/wp-content/uploads/2020/04/Keto-Stuffed-Cabbage-Rolls-Gluten-Free-Low-Carb-Recipe-10.jpg'),
-        new Recipe('Another test recipe', 'This is simply a test', 'https://www.blondelish.com/wp-content/uploads/2020/04/Keto-Stuffed-Cabbage-Rolls-Gluten-Free-Low-Carb-Recipe-10.jpg')
+        new Recipe('A test recipe', 
+        'Snitel cu ce trebuie!', 
+        'https://bucatelenoastre.ro/wp-content/uploads/2017/02/Snitel-din-piept-de-pui-cu-ou-si-pesmet.jpg',
+         [
+          new Ingredient('Meat', 1),
+          new Ingredient('French Fries', 20)   
+         ]),
+        new Recipe('Another test recipe',
+         'Mare burger pentru grasu!',
+          'https://dafjunior.ro/wp-content/uploads/2020/08/DAF-burger-dan-1.jpg', 
+          [
+             new Ingredient('Buns', 2),
+             new Ingredient('Meat', 1) 
+          ])
       ];
 
       getRecipes(){
