fb80dbc3b56cfaa0a5ad93a2e7ac13b4073f3b05
MuresanIA <muresan.alexandru933@gmail.com>
2021-05-13 14:51:49 +0300

    Adding the Shopping List Service

--- shopping-list-app/src/app/app.module.ts
+++ shopping-list-app/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- shopping-list-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ shopping-list-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,13 +1,12 @@
 import {
   Component,
   ElementRef,
-  EventEmitter,
   OnInit,
-  Output,
-  ViewChild,
+  ViewChild
 } from '@angular/core';
 
 import { Ingredient } from '../../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -19,17 +18,16 @@ export class ShoppingEditComponent implements OnInit {
   nameInputRef!: ElementRef;
   @ViewChild('amountInput')
   amountInputRef!: ElementRef;
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
-   
 
-  constructor() {}
 
-  ngOnInit(): void {}
+  constructor(private slService: ShoppingListService) { }
+
+  ngOnInit(): void { }
 
   onAddItem() {
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName, ingAmount);
-    this.ingredientAdded.emit(newIngredient);
+    this.slService.addIngredient(newIngredient)
   }
 }
