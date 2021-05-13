7b8510f90a79b4a5576ac2369da5040c83e55d79
MuresanIA <muresan.alexandru933@gmail.com>
2021-05-13 13:15:18 +0300

    Using a Service for Cross-Component

--- shopping-list-app/src/app/recipes/recipe.service.ts
+++ shopping-list-app/src/app/recipes/recipe.service.ts
@@ -1,10 +1,12 @@
+import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
+recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [
         new Recipe('A test recipe', 'This is simply a test', 'https://www.blondelish.com/wp-content/uploads/2020/04/Keto-Stuffed-Cabbage-Rolls-Gluten-Free-Low-Carb-Recipe-10.jpg'),
         new Recipe('Another test recipe', 'This is simply a test', 'https://www.blondelish.com/wp-content/uploads/2020/04/Keto-Stuffed-Cabbage-Rolls-Gluten-Free-Low-Carb-Recipe-10.jpg')
-      
       ];
 
       getRecipes(){
