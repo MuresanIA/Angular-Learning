652e60a5104d960b8746aa423eb49422869c92d1
MuresanIA <muresan.alexandru933@gmail.com>
2021-05-19 14:01:21 +0300

    Configuring Route Parameters

--- shopping-list-app/src/app/recipes/recipe.service.ts
+++ shopping-list-app/src/app/recipes/recipe.service.ts
@@ -8,14 +8,14 @@ export class RecipeService {
 recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A test recipe', 
+        new Recipe('Snitel ca la bunica acasa', 
         'Snitel cu ce trebuie!', 
         'https://bucatelenoastre.ro/wp-content/uploads/2017/02/Snitel-din-piept-de-pui-cu-ou-si-pesmet.jpg',
          [
           new Ingredient('Meat', 1),
           new Ingredient('French Fries', 20)   
          ]),
-        new Recipe('Another test recipe',
+        new Recipe('Burger A la Grasu',
          'Mare burger pentru grasu!',
           'https://dafjunior.ro/wp-content/uploads/2020/08/DAF-burger-dan-1.jpg', 
           [
@@ -28,6 +28,10 @@ recipeSelected = new EventEmitter<Recipe>();
 
       getRecipes(){
           return this.recipes.slice();
+      }
+
+      getRecipe(index: number){
+          return this.recipes[index];
       }
 
       addIngredientsToShoppingList(ingredients: Ingredient[]){
