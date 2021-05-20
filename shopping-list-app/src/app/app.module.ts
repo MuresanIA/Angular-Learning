93ba2b363ca2959bd4003d6b3ffd01ee6327fd2d
MuresanIA <muresan.alexandru933@gmail.com>
2021-05-20 12:19:20 +0300

    Adding Editing Routes

--- shopping-list-app/src/app/app.module.ts
+++ shopping-list-app/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
     ShoppingEditComponent,
     ShoppingListComponent,
     DropdownDirective,
-    RecipeStartComponent  
+    RecipeStartComponent,
+    RecipeEditComponent  
   ],
   imports: [
     BrowserModule,
