a9444a51f4d1eb5b02c44ecdeeb9bd8b3724d1ed
MuresanIA <muresan.alexandru933@gmail.com>
2021-05-31 13:17:04 +0300

    refine project with forms

--- shopping-list-app/src/app/recipes/recipe.service.ts
+++ shopping-list-app/src/app/recipes/recipe.service.ts
@@ -1,39 +1,58 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+
+import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
-import { Recipe } from './recipe.model';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe('Snitel ca la bunica acasa',
-            'Snitel cu ce trebuie!',
-            'https://bucatelenoastre.ro/wp-content/uploads/2017/02/Snitel-din-piept-de-pui-cu-ou-si-pesmet.jpg',
-            [
-                new Ingredient('Meat', 1),
-                new Ingredient('French Fries', 20)
-            ]),
-        new Recipe('Burger A la Grasu',
-            'Mare burger pentru grasu!',
-            'https://dafjunior.ro/wp-content/uploads/2020/08/DAF-burger-dan-1.jpg',
-            [
-                new Ingredient('Buns', 2),
-                new Ingredient('Meat', 1)
-            ])
-    ];
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('Big Fat Burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
+  ];
 
-    constructor(private slService: ShoppingListService) { }
+  constructor(private slService: ShoppingListService) {}
 
-    getRecipes() {
-        return this.recipes.slice();
-    }
+  getRecipes() {
+    return this.recipes.slice();
+  }
 
-    getRecipe(index: number) {
-        return this.recipes[index];
-    }
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
 
-    addIngredientsToShoppingList(ingredients: Ingredient[]) {
-        this.slService.addIngredients(ingredients);
-    }
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
 }
--- shopping-list-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ shopping-list-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,11 +1,11 @@
 import {
   Component,
-  OnDestroy,
   OnInit,
+  OnDestroy,
   ViewChild
 } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Subscription } from 'rxjs';
+import { Subscription } from 'rxjs/Subscription';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
@@ -13,44 +13,56 @@ import { ShoppingListService } from '../shopping-list.service';
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css'],
+  styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
-  @ViewChild('f') slForm!: NgForm;
-  subscription!: Subscription;
+  @ViewChild('f', { static: false }) slForm: NgForm;
+  subscription: Subscription;
   editMode = false;
-  editedItemIndex!: number;
-
-  editedItem!: Ingredient;
+  editedItemIndex: number;
+  editedItem: Ingredient;
 
   constructor(private slService: ShoppingListService) { }
 
-  ngOnInit(): void { 
+  ngOnInit() {
     this.subscription = this.slService.startedEditing
-    .subscribe(
-      (index: number) => {
-        this.editedItemIndex = index;
-        this.editMode = true;
-        this.editedItem = this.slService.getIngredient(index);
-        this.slForm.setValue({
-          name: this.editedItem,
-          amount: this.editedItem.amount
-        })
-      }
-    );
+      .subscribe(
+        (index: number) => {
+          this.editedItemIndex = index;
+          this.editMode = true;
+          this.editedItem = this.slService.getIngredient(index);
+          this.slForm.setValue({
+            name: this.editedItem.name,
+            amount: this.editedItem.amount
+          })
+        }
+      );
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
-    if(this.editMode){
+    if (this.editMode) {
       this.slService.updateIngredient(this.editedItemIndex, newIngredient);
     } else {
-      this.slService.addIngredient(newIngredient)
+      this.slService.addIngredient(newIngredient);
     }
+    this.editMode = false;
+    form.reset();
   }
 
-  ngOnDestroy(): void {
+  onClear() {
+    this.slForm.reset();
+    this.editMode = false;
+  }
+
+  onDelete() {
+    this.slService.deleteIngredient(this.editedItemIndex);
+    this.onClear();
+  }
+
+  ngOnDestroy() {
     this.subscription.unsubscribe();
   }
+
 }
