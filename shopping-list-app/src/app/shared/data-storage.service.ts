82d9539dc092db99440086a67ed889103a65b5b7
MuresanIA <muresan.alexandru933@gmail.com>
2021-06-07 21:00:58 +0300

    Transforming Response Data

--- shopping-list-app/src/app/shared/data-storage.service.ts
+++ shopping-list-app/src/app/shared/data-storage.service.ts
@@ -1,24 +1,45 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { RecipeService } from "../recipes/recipe.service";
-import { Recipe } from "../recipes/recipe.model";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+import { map } from 'rxjs/operators';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class DataStorageService {
-    constructor(private http: HttpClient, private recipesService: RecipeService) {    }
+  constructor(
+    private http: HttpClient,
+    private recipesService: RecipeService
+  ) {}
 
-        storeRecipes() {
-            const recipes = this.recipesService.getRecipes();
-           return this.http.put('https://ng-course-recipe-book-edc52-default-rtdb.firebaseio.com/recipes.json',
-            recipes).
-            subscribe(response => {
-                console.log(response);
-            });
-        }
+  storeRecipes() {
+    const recipes = this.recipesService.getRecipes();
+    return this.http
+      .put(
+        'https://ng-course-recipe-book-edc52-default-rtdb.firebaseio.com/recipes.json',
+        recipes
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
 
-        fetchRecipes() {
-            this.http.get<Recipe[]>('https://ng-course-recipe-book-edc52-default-rtdb.firebaseio.com/recipes.json').subscribe(recipes => {
-                this.recipesService.setRecipes(recipes);
-            });
-        }
+  fetchRecipes() {
+    this.http
+      .get<Recipe[]>(
+        'https://ng-course-recipe-book-edc52-default-rtdb.firebaseio.com/recipes.json'
+      )
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        })
+      )
+      .subscribe((recipes) => {
+        this.recipesService.setRecipes(recipes);
+      });
+  }
 }
