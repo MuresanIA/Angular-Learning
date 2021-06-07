45dea4ffcffafdf0196b947cf2581cad274ab831
MuresanIA <muresan.alexandru933@gmail.com>
2021-06-07 21:20:21 +0300

    Resolving Data Before Loading

--- shopping-list-app/src/app/shared/data-storage.service.ts
+++ shopping-list-app/src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -24,7 +24,7 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http
+   return this.http
       .get<Recipe[]>(
         'https://ng-course-recipe-book-edc52-default-rtdb.firebaseio.com/recipes.json'
       )
@@ -36,10 +36,10 @@ export class DataStorageService {
               ingredients: recipe.ingredients ? recipe.ingredients : [],
             };
           });
+        }),
+        tap(recipes => {
+            this.recipesService.setRecipes(recipes);
         })
       )
-      .subscribe((recipes) => {
-        this.recipesService.setRecipes(recipes);
-      });
   }
 }
