2116cb3328d7039434d8005ef1b77b4cd0b9d493
MuresanIA <muresan.alexandru933@gmail.com>
2021-06-07 20:53:07 +0300

    Fetching Recipes

--- shopping-list-app/src/app/shared/data-storage.service.ts
+++ shopping-list-app/src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -12,6 +13,12 @@ export class DataStorageService {
             recipes).
             subscribe(response => {
                 console.log(response);
+            });
+        }
+
+        fetchRecipes() {
+            this.http.get<Recipe[]>('https://ng-course-recipe-book-edc52-default-rtdb.firebaseio.com/recipes.json').subscribe(recipes => {
+                this.recipesService.setRecipes(recipes);
             });
         }
 }
