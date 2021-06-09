7c39be3d74676b119318c757686086df47fc48ee
MuresanIA <muresan.alexandru933@gmail.com>
2021-06-09 12:52:10 +0300

    Preparing the Signup Request 1

--- shopping-list-app/src/app/auth/auth.service.ts
+++ shopping-list-app/src/app/auth/auth.service.ts
@@ -14,7 +14,7 @@ export class AuthService {
     constructor(private http: HttpClient){}
     
     signup(email: string, password: string){
-        return this.http.post(
+        return this.http.post<AuthResponseData>(
             'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDNnnKH0iGArK3Hm3-_E8qPnAT6amc55Bw',
         {
             email: email,
