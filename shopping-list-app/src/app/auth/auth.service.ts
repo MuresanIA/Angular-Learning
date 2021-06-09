98d0e45fa0822dfcd7f11e7e05663dad8c26185a
MuresanIA <muresan.alexandru933@gmail.com>
2021-06-09 12:51:16 +0300

    Preparing the Signup Request

---
+++ shopping-list-app/src/app/auth/auth.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+interface AuthResponseData {
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    expiresIn: string;
+    localId: string;
+}
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+    constructor(private http: HttpClient){}
+    
+    signup(email: string, password: string){
+        return this.http.post(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDNnnKH0iGArK3Hm3-_E8qPnAT6amc55Bw',
+        {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        );
+    }
+}
