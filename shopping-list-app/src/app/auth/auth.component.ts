f27ada8f8cf9e75c42d3e34fb85dc1adbcc3780a
MuresanIA <muresan.alexandru933@gmail.com>
2021-06-09 15:12:13 +0300

    Improving Error Handling

--- shopping-list-app/src/app/auth/auth.component.ts
+++ shopping-list-app/src/app/auth/auth.component.ts
@@ -23,17 +23,16 @@ export class AuthComponent {
     }
     const email = form.value.email;
     const password = form.value.password;
-
     this.isLoading = true;
     if (this.isLoginMode) {
-      // 
+      // ...
     } else {
       this.authService.signup(email, password).subscribe(resData => {
         console.log(resData);
         this.isLoading = false;
-      }, error => {
-        console.log(error);
-        this.error = 'An error occured!';
+      }, errorMessage => {
+        console.log(errorMessage);
+        this.error = errorMessage;
         this.isLoading = false;
       });
     }
--- shopping-list-app/src/app/auth/auth.service.ts
+++ shopping-list-app/src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-
+import { catchError } from "rxjs/operators";
+import { throwError } from "rxjs";
 interface AuthResponseData {
     idToken: string;
     email: string;
@@ -9,18 +10,28 @@ interface AuthResponseData {
     localId: string;
 }
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class AuthService {
-    constructor(private http: HttpClient){}
-    
-    signup(email: string, password: string){
+    constructor(private http: HttpClient) { }
+
+    signup(email: string, password: string) {
         return this.http.post<AuthResponseData>(
             'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDNnnKH0iGArK3Hm3-_E8qPnAT6amc55Bw',
-        {
-            email: email,
-            password: password,
-            returnSecureToken: true
-        }
-        );
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        ).pipe(catchError(errorRes => {
+            let errorMessage = 'An unknown error occured';
+            if (!errorRes.error || !errorRes.error.error) {
+                return throwError(errorMessage);
+            }
+            switch (errorRes.error.error.message) {
+                case 'EMAIL_EXISTS':
+                    errorMessage = 'This email exists already!';
+            }
+            return throwError(errorMessage);
+        }));
     }
 }
