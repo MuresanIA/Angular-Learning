cdac5f30ad5555820862cfb241ca6e67bd26d73c
MuresanIA <muresan.alexandru933@gmail.com>
2021-06-10 19:00:50 +0300

    save

--- shopping-list-app/src/app/auth/auth.service.ts
+++ shopping-list-app/src/app/auth/auth.service.ts
@@ -24,7 +24,7 @@ export class AuthService {
   signup(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDb0xTaRAoxyCgvaDF3kk5VYOsTwB_3o7Y',
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDNnnKH0iGArK3Hm3-_E8qPnAT6amc55Bw',
         {
           email: email,
           password: password,
@@ -47,7 +47,7 @@ export class AuthService {
   login(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDb0xTaRAoxyCgvaDF3kk5VYOsTwB_3o7Y',
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDNnnKH0iGArK3Hm3-_E8qPnAT6amc55Bw',
         {
           email: email,
           password: password,
