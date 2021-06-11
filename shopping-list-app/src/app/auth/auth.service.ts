ec073549b421964d05a3cdcc90090b1fb577dc53
MuresanIA <muresan.alexandru933@gmail.com>
2021-06-11 15:27:30 +0300

    Creating & Storing the User Data

--- shopping-list-app/src/app/auth/auth.service.ts
+++ shopping-list-app/src/app/auth/auth.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { catchError } from "rxjs/operators";
-import { throwError } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
+import { Subject, throwError } from "rxjs";
 import { EmailValidator } from "@angular/forms";
+import { User } from "./user.model";
 export interface AuthResponseData {
   idToken: string;
   email: string;
@@ -14,7 +15,10 @@ export interface AuthResponseData {
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+
+  user = new Subject<User>();
+
+  constructor(private http: HttpClient) { }
 
   signup(email: string, password: string) {
     return this.http
@@ -26,7 +30,15 @@ export class AuthService {
           returnSecureToken: true,
         }
       )
-      .pipe(catchError(this.handleError));
+      .pipe(catchError(this.handleError),
+        tap(resData => {
+          this.handleAuthentication(
+            resData.email,
+            resData.idToken,
+            resData.localId,
+            +resData.expiresIn,
+          );
+        }));
   }
 
   login(email: string, password: string) {
@@ -39,7 +51,25 @@ export class AuthService {
           returnSecureToken: true,
         }
       )
-      .pipe(catchError(this.handleError));
+      .pipe(catchError(this.handleError), tap(resData => {
+        this.handleAuthentication(
+          resData.email,
+          resData.idToken,
+          resData.localId,
+          +resData.expiresIn,
+        );
+      }));
+  }
+
+  private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(
+      email,
+      userId,
+      token,
+      expirationDate
+    );
+    this.user.next(user);
   }
 
   private handleError(errorRes: HttpErrorResponse) {
