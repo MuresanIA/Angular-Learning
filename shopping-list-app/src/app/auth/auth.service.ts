58daeaace6f62297cbedac987e53081bcdd63767
MuresanIA <muresan.alexandru933@gmail.com>
2021-06-14 12:03:31 +0300

    Adding Auto-Logout #2

--- shopping-list-app/src/app/auth/auth.service.ts
+++ shopping-list-app/src/app/auth/auth.service.ts
@@ -106,6 +106,7 @@ export class AuthService {
   }
 
   autoLogout(expirationDuration: number) {
+    console.log(expirationDuration);
    this.tokenExpirationTimer = setTimeout(() => {
       this.logout();
     }, expirationDuration);
