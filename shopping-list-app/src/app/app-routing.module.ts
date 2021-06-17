ee33b67585604eb9a39dd43216cd29250ba9bd6a
MuresanIA <muresan.alexandru933@gmail.com>
2021-06-17 15:37:38 +0300

    Preloading Lazy-Loaded Code

--- shopping-list-app/src/app/app-routing.module.ts
+++ shopping-list-app/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
-import { AuthComponent } from "./auth/auth.component";
 
 const appRoutes: Routes = [
   { path: "", redirectTo: "/recipes", pathMatch: "full" },
@@ -24,7 +23,7 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
