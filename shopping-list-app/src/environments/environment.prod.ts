75e1990c83886dc9f4ed69917d4182fdcc622a0a
MuresanIA <muresan.alexandru933@gmail.com>
2021-06-22 18:10:40 +0300

    adding environment variables

--- shopping-list-app/src/environments/environment.prod.ts
+++ shopping-list-app/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseAPIKey: 'AIzaSyDNnnKH0iGArK3Hm3-_E8qPnAT6amc55Bw'
 };
--- shopping-list-app/src/environments/environment.ts
+++ shopping-list-app/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseAPIKey: 'AIzaSyDNnnKH0iGArK3Hm3-_E8qPnAT6amc55Bw'
 };
 
 /*
