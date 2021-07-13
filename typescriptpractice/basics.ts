88aa1ad16863cbf519d1d9c746bab6af5941b1e5
MuresanIA <muresan.alexandru933@gmail.com>
2021-07-13 15:54:37 +0300

    Array & Object Types

---
+++ typescriptpractice/basics.ts
@@ -0,0 +1,42 @@
+//primitives
+
+
+
+let age: number;
+
+age = 28; 
+
+let userName: string;
+
+userName = "Alex";
+
+let isAwesome: boolean;
+
+isAwesome = true;
+
+
+//complex types: array and object types
+
+let hobbies: string[];
+
+hobbies = ['Sports', 'Cooking'];
+
+let person: {
+    name: string;
+    age: number;
+};
+
+person = {
+    name: 'Alex',
+    age: 28
+};
+
+// person = {
+//     isEmployee: true
+// };
+
+
+let people: {
+    name: string;
+    age: number;
+}[];
