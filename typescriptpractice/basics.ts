176c31c3cf5c4e4a2c7c76f03ecbb8e16f6af3b5
MuresanIA <muresan.alexandru933@gmail.com>
2021-07-13 18:05:51 +0300

    Classes & TypeScript Go

--- typescriptpractice/basics.ts
+++ typescriptpractice/basics.ts
@@ -20,11 +20,10 @@ let hobbies: string[];
 
 hobbies = ["Sports", "Cooking"];
 
-
-type Person =  {
-    name: string;
-    age: number;
-  };
+type Person = {
+  name: string;
+  age: number;
+};
 
 let person: Person;
 
@@ -41,33 +40,58 @@ let people: Person[];
 
 // Type inference
 
-let course: string | number = 'React - The Complete Guide';
+let course: string | number = "React - The Complete Guide";
 
 course = 12341;
 
-
 // Functions & types
 
-function add2(a: number , b: number) { 
-    return a + b;
+function add2(a: number, b: number) {
+  return a + b;
 }
 
-function print(value: any){
-    console.log(value);
+function print(value: any) {
+  console.log(value);
 }
 
 //Generics
 
-function insertAtBeginning<T>(array: T[], value: T){
-    const newArray = [value, ...array];
-    return newArray;
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
 }
 
-const demoArray = [1,2,3];
+const demoArray = [1, 2, 3];
 
 const updateArray = insertAtBeginning(demoArray, -1); // [-1 ,1 ,2, 3]
 
+const stringArray = insertAtBeginning(["a", "b", "c"], "d");
 
-const stringArray = insertAtBeginning(['a', 'b', 'c'], 'd');
+class Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  private courses: string[];
 
+  constructor(first: string, last: string, age: number, courses: string[]) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.courses = courses;
+  }
 
+  enrol(courseName: string) {
+    this.courses.push(courseName);
+  }
+
+  listCourses() {
+    return this.courses.slice();
+  }
+}
+
+const student = new Student("Alex", "Cool", 28, ["Angular"]);
+student.enrol("React");
+
+student.listCourses(); // Angular, React
+
+// student.courses => Angular, React
